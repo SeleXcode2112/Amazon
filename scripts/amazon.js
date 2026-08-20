6784fb3d2773c298bef23eeb841558b6838f5848
SeleXcode <solomontareke41@gmail.com>
2026-08-20 20:45:29 +0300

    make the add to cart button intractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -55,6 +55,25 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    console.log("Button");
+    const productId = button.dataset.productId;
+
+      let matchingItem;
+    cart.forEach((item)=>{
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    })
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    
+    console.log(cart);
   });
 });
