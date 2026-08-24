a7a0f0f7dc0bc65c983fb854db1d2619d034774a
SeleXcode <solomontareke41@gmail.com>
2026-08-24 09:04:45 +0300

    Add jasmine testing freamwork

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,20 +1,20 @@
 import formatCurrency from "../scripts/utils/money.js";
 
-console.log('test suit: FormatCurrency');
+console.log("test suit: FormatCurrency");
 
-console.log('convert cents into dollars');
+console.log("convert cents into dollars");
 if (formatCurrency(2095) === "20.95") {
   console.log("passed");
 } else {
   console.log("failed");
 }
-console.log('works with 0 ');
+console.log("works with 0 ");
 if (formatCurrency(0) === "0.00") {
   console.log("passed");
 } else {
   console.log("failed");
 }
-console.log('rounds up to the nearest cent');
+console.log("rounds up to the nearest cent");
 if (formatCurrency(2000.5) === "20.01") {
   console.log("passed");
 } else {
