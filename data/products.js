c808b01d4efc883f7408ee12dcef533e5a4c8352
SeleXcode <solomontareke41@gmail.com>
2026-08-27 15:52:49 +0300

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -56,14 +56,35 @@ class Clothing extends Product {
 // logThi()
 // logThi.call('hello')
 
-  // const object3 = {
-  //   method: ()=>{
-  //     console.log(this);
-  //   }
-  // }
+// const object3 = {
+//   method: ()=>{
+//     console.log(this);
+//   }
+// }
 
-  // object3.method()
+// object3.method()
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log("load products");
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -541,3 +562,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
